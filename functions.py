ebee5d47620a63b35d2eb94651691f423358f5c7
Dallasz <grave-theboss@hotmail.com>
2022-01-14 21:24:50 -0300

    function added

--- functions.py
+++ functions.py
@@ -1,2 +1,6 @@
 def myAll(func, iterable):
     return all(func(x) for x in iterable)
+
+
+def filterStudents(func, iterable):
+    return list(filter(func, iterable))
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from inputs import inputGrade, inputName
+from functions import filterStudents
+
 
 def main():
     students = []
     
-    for i in range(1, 3):
+    for i in range(1, 4):
         students.append(
             {
                 'name': inputName('Digite o nome do aluno [%d]: ' % (i)),
@@ -12,6 +14,9 @@ def main():
         )
 
     print(students)
-    print('updated')
+
+    # updated 14/04 - worked!
+    filteredStudents = filterStudents(lambda x: sum(x['grades']) / len(x['grades']) >= 6, students)
+    print(filteredStudents)
     
 main()
